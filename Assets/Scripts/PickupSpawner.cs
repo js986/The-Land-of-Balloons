1b8087b4dbbf37cde59da339b913622a4868b53c
Joao Amaral <jja42@njit.edu>
2021-01-30 17:44:07 -0500

    Grid System and Modified Spawning

--- Assets/Scripts/PickupSpawner.cs
+++ Assets/Scripts/PickupSpawner.cs
@@ -4,42 +4,105 @@ using UnityEngine;
 
 public class PickupSpawner : MonoBehaviour
 {
-    float altitude;
-    float box_bottom_bound;
-    float box_left_bound;
-    float box_right_bound;
-    float box_top_bound;
-    float rand_x;
-    float rand_y;
+    [System.Serializable]
+    public struct GridSquare
+    {
+        public int bottom_bound;
+        public int left_bound;
+        public int right_bound;
+        public int top_bound;
+        public GridSquare(int top, int bottom,int left,int right)
+        {
+            this.bottom_bound = bottom;
+            this.left_bound = left;
+            this.right_bound = right;
+            this.top_bound = top;
+        }
+    }
+    [SerializeField] public List<GridSquare> grid;
+    Vector3 position;
+    int rand_x;
+    int rand_y;
     int rand_num;
     int rand_type;
     public GameObject Red_Gas;
     public GameObject Blue_Gas;
     public GameObject Green_Gas;
+    GridSquare current;
     // Start is called before the first frame update
     void Start()
     {
-        altitude = 0;
-        SpawnPickups(0);
+        current = new GridSquare(20, -20, -20, 20);
+        SpawnPickups(current);
+        grid = new List<GridSquare>();
+        grid.Add(current);
     }
 
     // Update is called once per frame
     void Update()
     {
-        altitude = (int)this.transform.position.y;
+        position = this.transform.position;
+        if(position.x > current.right_bound)
+        {
+            current = new GridSquare(current.top_bound, current.bottom_bound, current.left_bound+40, current.right_bound + 40);
+            if (!grid.Contains(current))
+            {
+                grid.Add(current);
+                SpawnPickups(current);
+            }
+        }
+        if (position.x < current.left_bound)
+        {
+            current = new GridSquare(current.top_bound, current.bottom_bound, current.left_bound-40, current.right_bound-40);
+            if (!grid.Contains(current))
+            {
+                grid.Add(current);
+                SpawnPickups(current);
+            }
+        }
+        if (position.y > current.top_bound)
+        {
+            current = new GridSquare(current.top_bound+40, current.bottom_bound+40, current.left_bound, current.right_bound);
+            if (!grid.Contains(current))
+            {
+                grid.Add(current);
+                SpawnPickups(current);
+            }
+        }
+        if (position.y < current.bottom_bound)
+        {
+            current = new GridSquare(current.top_bound-40, current.bottom_bound-40, current.left_bound, current.right_bound);
+            if (!grid.Contains(current))
+            {
+                grid.Add(current);
+                SpawnPickups(current);
+            }
+        }
+
     }
 
-    void SpawnPickups(int altitude)
+    void SpawnPickups(GridSquare current)
     {
-        box_bottom_bound = altitude - 10;
-        box_top_bound = altitude + 10;
-        box_left_bound = -10;
-        box_right_bound = 10;
-        rand_num = Random.Range(10, 20);
+        List<Vector3> pos_arr = new List<Vector3>();
+        Vector3 pos;
+        rand_num = Random.Range(10, 21);
         for (int i = 0; i < rand_num; i++)
         {
-            rand_x = Random.Range(box_left_bound, box_right_bound);
-            rand_y = Random.Range(box_bottom_bound, box_top_bound);
+            do
+            {
+                rand_x = Random.Range(current.left_bound+20, current.right_bound);
+                rand_y = Random.Range(current.bottom_bound, current.top_bound);
+                if(rand_x%2 != 0)
+                {
+                    rand_x++;
+                }
+                if (rand_y % 2 != 0)
+                {
+                    rand_y++;
+                }
+                pos = new Vector3(rand_x, rand_y, 0);
+            } while (pos_arr.Contains(pos));
+            pos_arr.Add(pos);
             rand_type = Random.Range(1, 11);
             if(rand_type == 10)
             {
@@ -54,6 +117,45 @@ public class PickupSpawner : MonoBehaviour
                 continue;
             }
             if (rand_type <6)
+            {
+                GameObject Gas = Instantiate(Red_Gas);
+                Gas.transform.position = new Vector3(rand_x, rand_y, 0);
+                continue;
+            }
+        }
+        
+        rand_num = Random.Range(10, 21);
+        for (int i = 0; i < rand_num; i++)
+        {
+            do
+            {
+                rand_x = Random.Range(current.left_bound, current.right_bound-20);
+                rand_y = Random.Range(current.bottom_bound, current.top_bound);
+                if (rand_x % 2 != 0)
+                {
+                    rand_x++;
+                }
+                if (rand_y % 2 != 0)
+                {
+                    rand_y++;
+                }
+                pos = new Vector3(rand_x, rand_y, 0);
+            } while (pos_arr.Contains(pos));
+            pos_arr.Add(pos);
+            rand_type = Random.Range(1, 11);
+            if (rand_type == 10)
+            {
+                GameObject Gas = Instantiate(Blue_Gas);
+                Gas.transform.position = new Vector3(rand_x, rand_y, 0);
+                continue;
+            }
+            if (rand_type > 6)
+            {
+                GameObject Gas = Instantiate(Green_Gas);
+                Gas.transform.position = new Vector3(rand_x, rand_y, 0);
+                continue;
+            }
+            if (rand_type < 6)
             {
                 GameObject Gas = Instantiate(Red_Gas);
                 Gas.transform.position = new Vector3(rand_x, rand_y, 0);
