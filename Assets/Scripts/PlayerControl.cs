a1414af5d446987c8e73d3d49438ccf4ff23c99c
uras <uo7@njit.edu>
2021-01-30 01:01:23 -0500

    Added movement scale for player

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,8 +6,10 @@ using UnityEngine.InputSystem;
 public class PlayerControl : MonoBehaviour
 {
     MasterControls input;
-    float _horizontalInput;
-    float _verticalInput;
+
+    Vector2 _movementInput;
+
+    [SerializeField] Vector2 _movementSpeedScale = new Vector2(1,1);
     Rigidbody2D _rb;
 
     void Awake()
@@ -37,23 +39,26 @@ public class PlayerControl : MonoBehaviour
     }
 
     void FixedUpdate(){
-        transform.Translate(_horizontalInput, _verticalInput, 0);
+        transform.Translate(
+            _movementInput.x * _movementSpeedScale.x, 
+            _movementInput.y * _movementSpeedScale.y, 
+            0);
     }
 
     #region Action Handlers
     void OnHorizontalMovement(InputAction.CallbackContext ctx){
 
-        _horizontalInput = ctx.ReadValue<float>();
+        _movementInput.x = ctx.ReadValue<float>();
     }
 
     void OnVerticalMovement(InputAction.CallbackContext ctx){
-        _verticalInput = ctx.ReadValue<float>();
+        _movementInput.y = ctx.ReadValue<float>();
         _rb.isKinematic = true;
         _rb.velocity = Vector2.zero;
     }
 
     void OffVerticalMovement(InputAction.CallbackContext ctx){
-        _verticalInput = 0;
+        _movementInput.y = 0;
         _rb.isKinematic = false;
     }
     #endregion
