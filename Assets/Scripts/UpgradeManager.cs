9a6c7ec94f3a24dc0799c4ce9b00bb048e80f6d2
Jordan Sefah <js986@njit.edu>
2021-01-30 15:49:03 -0500

    added ui for fuel and defense

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeManager : MonoBehaviour
 {
+    public PlayerControl pc;
     public float defense = 5f;
+    public float max_defense = 5f;
     public float fuel_efficiency = 5f;
+    public float max_fuel_efficiency = 5f;
     public int defense_level = 1;
     public int fuel_level = 1;
 
+    public Image fuel_bar;
+    public Image defense_bar;
+
     //Upgrades the defense of the players balloon; dependent on level
+
+    private void Start()
+    {
+        pc = this.GetComponent<PlayerControl>();
+    }
     void UpgradeDefense()
     {
         defense += 2f;
