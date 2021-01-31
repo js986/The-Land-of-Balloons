7c25ad4261362affa81a2a472f5684adaa8df191
uras <uo7@njit.edu>
2021-01-31 13:01:43 -0500

    Added region system to grid manager Moved scripts int more folders Applied some stuff to prefabs

---
+++ Assets/Scripts/Region.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Region{
+    
+    public string name;
+    public float lowerEntryBound;
+    public float upperExitBound;
+}
