b22562304aa3696fee90e0345fd40aeb05ccf2b0
uras <uo7@njit.edu>
2021-01-31 14:20:00 -0500

    Changed alchemy input and re-arranged ui

--- Assets/Scripts/Input/MasterControls.cs
+++ Assets/Scripts/Input/MasterControls.cs
@@ -25,6 +25,54 @@ public class @MasterControls : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Vector2"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Refuel"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""addd715d-b422-4d7f-8fdb-f15f0f883867"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Repair"",
+                    ""type"": ""Button"",
+                    ""id"": ""d22e37d7-6411-4d4e-917e-b5d9c8c236b5"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""UpgradeDefense"",
+                    ""type"": ""Button"",
+                    ""id"": ""df18247b-4387-4ab3-b6d1-0d270dbf60ae"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""UpgradeEngine"",
+                    ""type"": ""Button"",
+                    ""id"": ""e2f83f50-24fb-4a28-8e4b-4a00289648fa"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""ConvertGas"",
+                    ""type"": ""Button"",
+                    ""id"": ""c0672b5f-f197-441e-bb6e-ac657110ccd3"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""SwitchGas"",
+                    ""type"": ""Button"",
+                    ""id"": ""21c2a442-9a96-4d33-b12c-22b2f0e2a5be"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -137,6 +185,160 @@ public class @MasterControls : IInputActionCollection, IDisposable
                     ""action"": ""Movement"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""69085e86-2243-4f57-8dfb-81eddadb4b30"",
+                    ""path"": ""<Keyboard>/1"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Refuel"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""75ad1333-61bf-4774-ba16-9dae81364117"",
+                    ""path"": ""<Keyboard>/u"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Refuel"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d6f7fbe2-9116-49a1-b930-95e01b711197"",
+                    ""path"": ""<Keyboard>/2"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Repair"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f15ab6f5-8f8a-4fa0-bafd-cc5d7aa79d55"",
+                    ""path"": ""<Keyboard>/i"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Repair"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""de9debce-19b1-4122-99e0-5cc5b7871d77"",
+                    ""path"": ""<Keyboard>/3"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""UpgradeDefense"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""fbd46fed-c34d-4654-8069-1f2f4261a776"",
+                    ""path"": ""<Keyboard>/o"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""UpgradeDefense"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""2f4af7fa-342e-4c84-b4ab-a93ce91c3621"",
+                    ""path"": ""<Keyboard>/4"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""UpgradeEngine"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5d446eaa-5764-433a-9c8e-1abfdd4b7fbd"",
+                    ""path"": ""<Keyboard>/p"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""UpgradeEngine"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""1D Axis"",
+                    ""id"": ""fb036aef-99a4-4cf4-b26e-d3389d1829bd"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ConvertGas"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""7332771a-4a20-485c-806f-77f8f5d50e2c"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ConvertGas"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""986510d3-7f9b-48f4-9201-fb6c28ea0f11"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ConvertGas"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""1D Axis"",
+                    ""id"": ""7093c4cf-bb7c-4d08-9ce3-5a4591d86dac"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SwitchGas"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""8620d082-8f09-461e-8056-0f769b252683"",
+                    ""path"": ""<Keyboard>/q"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SwitchGas"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""c19e5fc7-687e-481e-ba92-c01bfefd9787"",
+                    ""path"": ""<Keyboard>/e"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SwitchGas"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
                 }
             ]
         }
@@ -146,6 +348,12 @@ public class @MasterControls : IInputActionCollection, IDisposable
         // Main
         m_Main = asset.FindActionMap("Main", throwIfNotFound: true);
         m_Main_Movement = m_Main.FindAction("Movement", throwIfNotFound: true);
+        m_Main_Refuel = m_Main.FindAction("Refuel", throwIfNotFound: true);
+        m_Main_Repair = m_Main.FindAction("Repair", throwIfNotFound: true);
+        m_Main_UpgradeDefense = m_Main.FindAction("UpgradeDefense", throwIfNotFound: true);
+        m_Main_UpgradeEngine = m_Main.FindAction("UpgradeEngine", throwIfNotFound: true);
+        m_Main_ConvertGas = m_Main.FindAction("ConvertGas", throwIfNotFound: true);
+        m_Main_SwitchGas = m_Main.FindAction("SwitchGas", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -196,11 +404,23 @@ public class @MasterControls : IInputActionCollection, IDisposable
     private readonly InputActionMap m_Main;
     private IMainActions m_MainActionsCallbackInterface;
     private readonly InputAction m_Main_Movement;
+    private readonly InputAction m_Main_Refuel;
+    private readonly InputAction m_Main_Repair;
+    private readonly InputAction m_Main_UpgradeDefense;
+    private readonly InputAction m_Main_UpgradeEngine;
+    private readonly InputAction m_Main_ConvertGas;
+    private readonly InputAction m_Main_SwitchGas;
     public struct MainActions
     {
         private @MasterControls m_Wrapper;
         public MainActions(@MasterControls wrapper) { m_Wrapper = wrapper; }
         public InputAction @Movement => m_Wrapper.m_Main_Movement;
+        public InputAction @Refuel => m_Wrapper.m_Main_Refuel;
+        public InputAction @Repair => m_Wrapper.m_Main_Repair;
+        public InputAction @UpgradeDefense => m_Wrapper.m_Main_UpgradeDefense;
+        public InputAction @UpgradeEngine => m_Wrapper.m_Main_UpgradeEngine;
+        public InputAction @ConvertGas => m_Wrapper.m_Main_ConvertGas;
+        public InputAction @SwitchGas => m_Wrapper.m_Main_SwitchGas;
         public InputActionMap Get() { return m_Wrapper.m_Main; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -213,6 +433,24 @@ public class @MasterControls : IInputActionCollection, IDisposable
                 @Movement.started -= m_Wrapper.m_MainActionsCallbackInterface.OnMovement;
                 @Movement.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnMovement;
                 @Movement.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnMovement;
+                @Refuel.started -= m_Wrapper.m_MainActionsCallbackInterface.OnRefuel;
+                @Refuel.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnRefuel;
+                @Refuel.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnRefuel;
+                @Repair.started -= m_Wrapper.m_MainActionsCallbackInterface.OnRepair;
+                @Repair.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnRepair;
+                @Repair.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnRepair;
+                @UpgradeDefense.started -= m_Wrapper.m_MainActionsCallbackInterface.OnUpgradeDefense;
+                @UpgradeDefense.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnUpgradeDefense;
+                @UpgradeDefense.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnUpgradeDefense;
+                @UpgradeEngine.started -= m_Wrapper.m_MainActionsCallbackInterface.OnUpgradeEngine;
+                @UpgradeEngine.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnUpgradeEngine;
+                @UpgradeEngine.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnUpgradeEngine;
+                @ConvertGas.started -= m_Wrapper.m_MainActionsCallbackInterface.OnConvertGas;
+                @ConvertGas.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnConvertGas;
+                @ConvertGas.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnConvertGas;
+                @SwitchGas.started -= m_Wrapper.m_MainActionsCallbackInterface.OnSwitchGas;
+                @SwitchGas.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnSwitchGas;
+                @SwitchGas.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnSwitchGas;
             }
             m_Wrapper.m_MainActionsCallbackInterface = instance;
             if (instance != null)
@@ -220,6 +458,24 @@ public class @MasterControls : IInputActionCollection, IDisposable
                 @Movement.started += instance.OnMovement;
                 @Movement.performed += instance.OnMovement;
                 @Movement.canceled += instance.OnMovement;
+                @Refuel.started += instance.OnRefuel;
+                @Refuel.performed += instance.OnRefuel;
+                @Refuel.canceled += instance.OnRefuel;
+                @Repair.started += instance.OnRepair;
+                @Repair.performed += instance.OnRepair;
+                @Repair.canceled += instance.OnRepair;
+                @UpgradeDefense.started += instance.OnUpgradeDefense;
+                @UpgradeDefense.performed += instance.OnUpgradeDefense;
+                @UpgradeDefense.canceled += instance.OnUpgradeDefense;
+                @UpgradeEngine.started += instance.OnUpgradeEngine;
+                @UpgradeEngine.performed += instance.OnUpgradeEngine;
+                @UpgradeEngine.canceled += instance.OnUpgradeEngine;
+                @ConvertGas.started += instance.OnConvertGas;
+                @ConvertGas.performed += instance.OnConvertGas;
+                @ConvertGas.canceled += instance.OnConvertGas;
+                @SwitchGas.started += instance.OnSwitchGas;
+                @SwitchGas.performed += instance.OnSwitchGas;
+                @SwitchGas.canceled += instance.OnSwitchGas;
             }
         }
     }
@@ -227,5 +483,11 @@ public class @MasterControls : IInputActionCollection, IDisposable
     public interface IMainActions
     {
         void OnMovement(InputAction.CallbackContext context);
+        void OnRefuel(InputAction.CallbackContext context);
+        void OnRepair(InputAction.CallbackContext context);
+        void OnUpgradeDefense(InputAction.CallbackContext context);
+        void OnUpgradeEngine(InputAction.CallbackContext context);
+        void OnConvertGas(InputAction.CallbackContext context);
+        void OnSwitchGas(InputAction.CallbackContext context);
     }
 }
